41a71a98a436629ed3cd0db261dc08519ea0597b
Perecharla Babi <perecharlababi47.com>
2025-01-09 20:22:28 +0530

    Sample data

---
+++ Sample Data Sets/scott/dept.js
@@ -0,0 +1,6 @@
+db.dept.insertMany([
+  { deptno: 10, dname: "accounting", loc: "new york" },
+  { deptno: 20, dname: "research", loc: "dallas" },
+  { deptno: 30, dname: "sales", loc: "chicago" },
+  { deptno: 40, dname: "operations", loc: "boston" },
+]);
