50c9516f9399e52cc44e75d70d229be1dd579aab
Perecharla Babi <perecharlababi47.com>
2025-01-10 10:52:48 +0530

    find method

--- commands/4. Doument  Commands/documents.js
+++ commands/4. Doument  Commands/documents.js
@@ -1,9 +1,10 @@
 //! === Documents ===
 // ? create -----> insert(), insertOne(), insertMany()
-// ? read
+// ? read   -----> find() , findOne()
 // ? update
 // ? delete
 
+// ! =================== Create ===================
 // ? >>> insert():
 // It will create new document(s) in the collection.
 // Syntax: db.collectionName.insert(doc);
@@ -111,3 +112,81 @@ db.emp.insertMany([
     deptno: 20,
   },
 ]);
+
+
+// ! =================== Read ===================
+// ? >>> find()
+// It will helps us to read the data.
+// Syntax: db.collection.find({filter} , {projection} ,{options});
+// filter --> will pass the filtering condition to the data ---> equal where clause in SQL
+// pojection --> will display the data --> equal to select clause in SQL
+// options --> to pass additional options
+
+// & WAQTD the details employees in emp collection.
+// SQL : SELECT * FROM EMP;
+// mongodb : db.emp.find();
+
+// & WAQTD the details departments in dept collection.
+// SQL: SELECT * FROM DEPT;
+// mongodb: db.dept.find();
+
+// & WAQTD ename , sal , Job of all the employees.
+// SQL:
+/*
+SELECT ENAME, SAL, JOB 
+FROM EMP;
+*/
+
+// mongodb:
+/* db.emp.find({}, { _id: 0, ename: 1, sal: 1, job: 1 }, {}); */
+
+// & WAQTD empno , deptno of all the employees
+// SQL:
+/*
+SELECT EMPNO, DEPTNO
+FROM EMP;
+*/
+
+// mongodb:
+/* db.emp.find({}, { _id: 0,  , empno:1 , deptno:1 }, {}); */
+
+// & WAQTD the employees details who are earning 1250.
+// SQL:
+/*
+SELECT * 
+FROM EMP
+WHERE SAL = 1250
+*/
+// mongodb:
+/* db.emp.find({ sal: 1250 }, {}, {}); */
+
+// & WAQTD the employees details who are working as manager
+// SQL:
+/*
+SELECT * 
+FROM EMP 
+WHERE JOB = "MANAGER"
+*/
+
+// mongodb:
+/* db.emp.find({ job: "manager" }, {}, {}); */
+
+// ? Task
+// & WAQTD the details of the employees who are in dept no 20
+// & WAQTD the details of the employees who are in dept no 10
+// & WAQTD the details of the employees who are in dept no 30
+// & WAQTD the details of the employees who are working as clerk
+// & WAQTD the details of the employees who are working as analyst
+// & WAQTD the details of the employees who are working as salesman
+// & WAQTD the details of the employees whose name is Scott
+// & WAQTD the details of the employees whose name is Turner
+// & WAQTD the details of the employees who are working as clerk in dept no 20.
+// & WAQTD the details of the employees who are working as manager in dept no 10.
+// & WAQTD the details of the employees who are working as salesman in dept no 30.
+// & WAQTD the details of the employees who are earning 3000 as a analyst.
+// & WAQTD the details of the employees who are reporting to 7698.
+// & WAQTD the details of the employees who are reporting to 7566.
+// & WAQTD the details of the employees who are earning comm of 500 exactly.
+// & WAQTD the manager no of martin.
+// & WAQTD Ford's name , job and hire date
+// & WAQTD Jones name , employee number and his manager number along with dept no.
